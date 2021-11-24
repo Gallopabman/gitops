c0137923c3f1a0f0e543d7b3170c93928c68d218
pgalleguillo <pablo.galleguillo@semperti.com>
2021-11-24 12:57:53 -0300

    emergency push

--- main.tf
+++ main.tf
@@ -28,51 +28,80 @@ data "huaweicloud_images_image" "myimage" {
 }
 
 ########## Create vpc & Network ##########
-
-resource "huaweicloud_vpc" "vpc" {
-  name = "vpc_pg"
+resource "huaweicloud_vpc" "pgvpc" {
+  name = "pgvpc"
   cidr = "192.168.0.0/16"
 }
+
 resource "huaweicloud_vpc_subnet" "pgsubnet" {
-  name       = "pgsubnet"
-  cidr       = "192.168.0.0/16"
-  gateway_ip = "192.168.0.1"
-  vpc_id     = huaweicloud_vpc.vpc.id
+  name              = "pgsubnet"
+  cidr              = "192.168.0.0/16"
+  gateway_ip        = "192.168.0.1"
+  vpc_id            = huaweicloud_vpc.pgvpc.id
 }
-resource "huaweicloud_vpc_eip" "pgeip" {
-  publicip {
-    type = "5_bgp"
-  }
-  bandwidth {
-    name        = "mybandwidth"
-    size        = 8
-    share_type  = "PER"
-    charge_mode = "traffic"
-  }
+
+resource "huaweicloud_nat_gateway" "pgnat" {
+  name                = "pgnat"
+  description         = "pg Nat"
+  spec                = "1"
+  router_id           = huaweicloud_vpc.pgvpc.id
+  internal_network_id = huaweicloud_vpc_subnet.pgsubnet.id
 }
-resource "huaweicloud_compute_eip_associate" "associated" {
-  public_ip   = huaweicloud_vpc_eip.pgeip.address
-  instance_id = huaweicloud_compute_instance.pgecs.id
+
+resource "huaweicloud_nat_snat_rule" "pgsnat" {
+  nat_gateway_id = huaweicloud_nat_gateway.pgnat.id
+  network_id     = huaweicloud_vpc_subnet.pgsubnet.id
+  floating_ip_id = huaweicloud_vpc_eip.pgeip.id
 }
-resource "huaweicloud_networking_secgroup" "mysecgroup" {
-  name        = "secgroup"
+
+########## SG ##########
+
+resource "huaweicloud_networking_secgroup" "pgsecgroup" {
+  name        = "pgsecgroup"
   description = "My security group"
 }
 
-resource "huaweicloud_networking_secgroup_rule" "secgroup_rule" {
-  security_group_id = huaweicloud_networking_secgroup.mysecgroup.id
+resource "huaweicloud_networking_secgroup_rule" "pgsecgroup_rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.pgsecgroup.id
+}
+
+resource "huaweicloud_networking_secgroup_rule" "rule-https" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.pgsecgroup.id
+}
+
+resource "huaweicloud_networking_secgroup_rule" "rule-http" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.pgsecgroup.id
+}
+
+resource "huaweicloud_networking_secgroup_rule" "rule-http1" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 8080
   port_range_max    = 8080
   remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.pgsecgroup.id
 }
 
 ########## Create ECS ##########
-data "huaweicloud_vpc_subnet" "pgsubnet" {
-  name = "subnet-pg"
-}
 
 resource "random_password" "password" {
   length           = 16
@@ -81,14 +110,32 @@ resource "random_password" "password" {
 }
 
 resource "huaweicloud_compute_instance" "pgecs" {
-  name              = "basic"
+  name              = "pgecs"
   admin_pass        = random_password.password.result
   image_id          = data.huaweicloud_images_image.myimage.id
   flavor_id         = data.huaweicloud_compute_flavors.myflavor.ids[0]
   availability_zone = data.huaweicloud_availability_zones.myaz.names[0]
-  security_groups   = ["mysecgroup"]
-
+  security_groups   = ["pgsecgroup"]
+  key_pair          = "my_key_pair_name"
+  
   network {
-    uuid = data.huaweicloud_vpc_subnet.pgsubnet.id
+    uuid = "huaweicloud_vpc_subnet.pgsubnet.id"
   }
+}
+
+resource "huaweicloud_vpc_eip" "pgeip" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name        = "test"
+    size        = 8
+    share_type  = "PER"
+    charge_mode = "traffic"
+  }
+}
+
+resource "huaweicloud_compute_eip_associate" "pgassociated" {
+  public_ip   = huaweicloud_vpc_eip.pgeip.address
+  instance_id = huaweicloud_compute_instance.pgecs.id
 }
