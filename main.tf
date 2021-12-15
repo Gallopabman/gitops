860dfeb380371f2d34eddfa2f3f9285cd1981db3
pgalleguillo <pablo.galleguillo@semperti.com>
2021-12-15 15:16:41 -0300

    ansible finished

--- main.tf
+++ main.tf
@@ -71,33 +71,33 @@ resource "huaweicloud_networking_secgroup_rule" "secgroup_rule" {
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
 }
-#resource "huaweicloud_networking_secgroup_rule" "https" {
-#  direction         = "ingress"
-#  ethertype         = "IPv4"
-#  protocol          = "tcp"
-#  port_range_min    = 443
-#  port_range_max    = 443
-#  remote_ip_prefix  = "0.0.0.0/0"
-#  security_group_id = huaweicloud_networking_secgroup.pg-secgroup.id
-#}
-#resource "huaweicloud_networking_secgroup_rule" "http" {
-#  direction         = "ingress"
-#  ethertype         = "IPv4"
-#  protocol          = "tcp"
-#  port_range_min    = 80
-#  port_range_max    = 80
-#  remote_ip_prefix  = "0.0.0.0/0"
-#  security_group_id = huaweicloud_networking_secgroup.pg-secgroup.id
-#}
-#resource "huaweicloud_networking_secgroup_rule" "http1" {
-#  direction         = "ingress"
-#  ethertype         = "IPv4"
-#  protocol          = "tcp"
-#  port_range_min    = 8080
-#  port_range_max    = 8080
-#  remote_ip_prefix  = "0.0.0.0/0"
-#  security_group_id = huaweicloud_networking_secgroup.pg-secgroup.id
-#}
+resource "huaweicloud_networking_secgroup_rule" "https" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.pg-secgroup.id
+}
+resource "huaweicloud_networking_secgroup_rule" "http" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.pg-secgroup.id
+}
+resource "huaweicloud_networking_secgroup_rule" "http1" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 8080
+  port_range_max    = 8080
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = huaweicloud_networking_secgroup.pg-secgroup.id
+}
 
 
 #########################
